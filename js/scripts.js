4f83d9bf61f66059d21d32ed2c4cf12e0413c290
Coronel Dalma <coronel.dg@gmail.com>
2024-12-04 15:31:35 -0300

    added search for items by category with pagination

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,27 @@ fetch('json/categories.json')
     .then(res => res.json())
     .then(data => {
         const categoryList=document.querySelector(".category-filter");
+
         data.forEach(category => {
             const iconClass= iconMap[category.slug] || "fa-question";
             categoryList.innerHTML += `
-                <li><a href="#" class="d-flex align-items-center justify-content-between"><span>${category.name}</span><i class="fa-solid ${iconClass} icon-link"></i></a></li>
+                <li><a href="${category.url}" class="category-link d-flex align-items-center justify-content-between"><span>${category.name}</span><i class="fa-solid ${iconClass} icon-link"></i></a></li>
             `;
         });
+
+        const categoryLinks = document.querySelectorAll('.category-link');
+        categoryLinks.forEach(link => {
+            link.addEventListener('click',function(event) {
+                event.preventDefault();
+
+                fetch(link.href)
+                .then(res => res.json())
+                .then(data => {
+                    displayData(data);
+                    displayPagination(Math.ceil(data.total / limit));
+                })
+            })
+        })
     });
 })
 
@@ -54,32 +69,71 @@ function displayData(data) {
     })
 }
 
-// function displayPagination(pages) {
-//     var pagination = document.querySelector(".pagination");
+var pageItems;
+var currentPage = 1;
+function displayPagination(pages) {
+    var pagination = document.querySelector(".pagination");
+    pagination.innerHTML = ``;
+    const disabled = pages === 1 ? "disabled" : "";
+    pagination.innerHTML +=  `
+        <li class="page-item arrow-item ${disabled}" >
+            <a class="page-link" href="#" aria-label="Previous" id="prevPage">
+                <span aria-hidden="true">&laquo;</span>
+            </a>
+        </li>
+    `;
 
-//     pagination.innerHTML +=  `
-//         <li class="page-item arrow-item" >
-//             <a class="page-link" href="#" aria-label="Previous" id="prevPage">
-//                 <span aria-hidden="true">&laquo;</span>
-//             </a>
-//         </li>
-//     `;
+    for (i= 1; i<=pages; i++) {
+        let activeClass = (i === 1) ? 'active' : '';
+        pagination.innerHTML +=  `
+            <li class="page-item ${activeClass} ${disabled}" data-page="${i}"><a class="page-link" href="#">${i}</a></li>
+        `;
+    }
 
-//     for (i= 1; i<=pages; i++) {
-//         let activeClass = (i === 1) ? 'active' : '';
-//         pagination.innerHTML +=  `
-//             <li class="page-item ${activeClass}" data-page="${i}"><a class="page-link" href="#">${i}</a></li>
-//         `;
-//     }
+    pagination.innerHTML += `
+        <li class="page-item arrow-item ${disabled}" >
+            <a class="page-link" href="#" aria-label="Next" id="nextPage">
+                <span aria-hidden="true">&raquo;</span>
+            </a>
+        </li>
+    `;
 
-//     pagination.innerHTML += `
-//         <li class="page-item arrow-item" >
-//             <a class="page-link" href="#" aria-label="Next" id="nextPage">
-//                 <span aria-hidden="true">&raquo;</span>
-//             </a>
-//         </li>
-//     `;
-// }
+    pageItems = document.querySelectorAll('.page-item');
+    currentPage = 1;
+
+    pageItems.forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            if (!item.classList.contains('arrow-item')) {
+                pageItems.forEach( i => i.classList.remove('active'));
+                item.classList.add('active');
+
+                currentPage = parseInt(item.getAttribute('data-page'));
+            }
+            updateArrows();
+            updateData(currentPage);
+        })
+    })
+
+    document.getElementById('prevPage').addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        if (currentPage > 1) {
+            currentPage--;
+            updatePagination();
+        }
+    });
+    
+    document.getElementById('nextPage').addEventListener('click', function(event) {
+        event.preventDefault();
+        if (currentPage < pages) { 
+            currentPage++;
+            updatePagination();
+        }
+    });
+    
+}
 
 const limit = 30;
 let pages = 0;
@@ -89,44 +143,10 @@ window.addEventListener("load", () => {
     .then(data => {
         displayData(data);
         pages = Math.ceil(data.total / limit);
-        // displayPagination(pages);
+        displayPagination(pages);
+        updateArrows();
     });
 })
-
-// Pagination
-const pageItems = document.querySelectorAll('.page-item');
-let currentPage = 1;
-
-pageItems.forEach(item => {
-    item.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        if (!item.classList.contains('arrow-item')) {
-            pageItems.forEach( i => i.classList.remove('active'));
-            item.classList.add('active');
-
-            currentPage = parseInt(item.getAttribute('data-page'));
-        }
-        updateArrows();
-        updateData(currentPage);
-    })
-})
-
-document.getElementById('prevPage').addEventListener('click', function(event) {
-    event.preventDefault();
-    if (currentPage > 1) {
-        currentPage--;
-        updatePagination();
-    }
-});
-
-document.getElementById('nextPage').addEventListener('click', function(event) {
-    event.preventDefault();
-    if (currentPage < pages) { 
-        currentPage++;
-        updatePagination();
-    }
-});
 
 function updateArrows() {
     var arrows = document.querySelectorAll(".arrow-item");
@@ -134,8 +154,8 @@ function updateArrows() {
         arrow.classList.remove('disabled');
     });
 
-    currentPage === 1 && arrows[0].classList.add('disabled');
-    currentPage === pages && arrows[1].classList.add('disabled');
+    (currentPage === 1 ) && arrows[0].classList.add('disabled');
+    (currentPage === pages) && arrows[1].classList.add('disabled');
 }
 function updatePagination() {
     pageItems.forEach(item => {
