16856cacc057dc04baf799f16325a24517cbf78d
Coronel Dalma <coronel.dg@gmail.com>
2024-12-04 12:24:23 -0300

    pagination with Js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,7 @@
-// Cargar los productos desde DummyJSON (ejemplo básico)
-// fetch('https://dummyjson.com/products')
-// .then(response => response.json())
-// .then(data => {
-//     const productCardsContainer = document.getElementById('product-cards');
-//     data.products.forEach(product => {
-//         productCardsContainer.innerHTML += `
-//             <div class="col-md-4">
-//                 <div class="card">
-//                     <img src="${product.images[0]}" class="card-img-top" alt="${product.title}">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${product.title}</h5>
-//                         <p class="card-text">${product.description}</p>
-//                         <a href="Product.html?id=${product.id}" class="btn btn-primary">Ver Producto</a>
-//                     </div>
-//                 </div>
-//             </div>
-//         `;
-//     });
-// });
-
-
+// Get products category list
 fetch('json/categories.json')
 .then(res => res.json())
 .then(iconMap => {
-    // Get products category list
     fetch('https://dummyjson.com/products/categories')
     .then(res => res.json())
     .then(data => {
@@ -36,5 +14,142 @@ fetch('json/categories.json')
         });
     });
 })
+
+function updateData(currentPage) {
+    // Get products
+    const from= limit * (currentPage-1);
+    fetch(`https://dummyjson.com/products?limit=${limit}&skip=${from}&select=title,price,rating,images,category`)
+    .then(res => res.json())
+    .then(data => {
+        displayData(data);
+    });
+}
+
+function displayData(data) {
+    const container = document.querySelector('.container-cards');
+    container.innerHTML = '';
+    data.products.forEach(product => {
+        container.innerHTML += `
+            <li class="my-card product-card">
+                <a href="product.html">
+                    <div class="image-container imageh-75">
+                        <div class="icons-product-card">
+                            <div class="star-icon">${product.rating}<i class="fa-solid fa-star"></i></div>
+                            <div class="price-tag">${product.price}</div>
+                        </div>
+                        <img src=${product.images[0]} alt=${product.title} class="card-image product-image">
+                    </div>
+                    <div class="info-product-card">
+                        <div class="info-card">
+                            <p class="white">${product.category}</p>
+                            <h6 class="product-card-title">${product.title}</h6>
+                        </div>
+                        <div class="card-btn">
+                            <button><i class="fa-solid fa-cart-plus fa-xl "></i></button>   
+                        </div>
+                    </div>
+                </a> 
+            </li>
+        `;
+    })
+}
+
+// function displayPagination(pages) {
+//     var pagination = document.querySelector(".pagination");
+
+//     pagination.innerHTML +=  `
+//         <li class="page-item arrow-item" >
+//             <a class="page-link" href="#" aria-label="Previous" id="prevPage">
+//                 <span aria-hidden="true">&laquo;</span>
+//             </a>
+//         </li>
+//     `;
+
+//     for (i= 1; i<=pages; i++) {
+//         let activeClass = (i === 1) ? 'active' : '';
+//         pagination.innerHTML +=  `
+//             <li class="page-item ${activeClass}" data-page="${i}"><a class="page-link" href="#">${i}</a></li>
+//         `;
+//     }
+
+//     pagination.innerHTML += `
+//         <li class="page-item arrow-item" >
+//             <a class="page-link" href="#" aria-label="Next" id="nextPage">
+//                 <span aria-hidden="true">&raquo;</span>
+//             </a>
+//         </li>
+//     `;
+// }
+
+const limit = 30;
+let pages = 0;
+window.addEventListener("load", () => {
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(data => {
+        displayData(data);
+        pages = Math.ceil(data.total / limit);
+        // displayPagination(pages);
+    });
+})
+
+// Pagination
+const pageItems = document.querySelectorAll('.page-item');
+let currentPage = 1;
+
+pageItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        if (!item.classList.contains('arrow-item')) {
+            pageItems.forEach( i => i.classList.remove('active'));
+            item.classList.add('active');
+
+            currentPage = parseInt(item.getAttribute('data-page'));
+        }
+        updateArrows();
+        updateData(currentPage);
+    })
+})
+
+document.getElementById('prevPage').addEventListener('click', function(event) {
+    event.preventDefault();
+    if (currentPage > 1) {
+        currentPage--;
+        updatePagination();
+    }
+});
+
+document.getElementById('nextPage').addEventListener('click', function(event) {
+    event.preventDefault();
+    if (currentPage < pages) { 
+        currentPage++;
+        updatePagination();
+    }
+});
+
+function updateArrows() {
+    var arrows = document.querySelectorAll(".arrow-item");
+    arrows.forEach(arrow => {
+        arrow.classList.remove('disabled');
+    });
+
+    currentPage === 1 && arrows[0].classList.add('disabled');
+    currentPage === pages && arrows[1].classList.add('disabled');
+}
+function updatePagination() {
+    pageItems.forEach(item => {
+        item.classList.remove('active');
+        var page = parseInt(item.getAttribute('data-page'));
+        if (page === currentPage) {
+            item.classList.add('active');
+        }
+    });
+    updateArrows();
+    updateData(currentPage);
+}
+
+
+
 
 
